ea4101e88945e4ccccfffa24a36162b75dff2aca
1101058129 <164818983+1101058129@users.noreply.github.com>
2024-11-08 14:11:10 +0800

    Add files via upload

--- login.js
+++ login.js
@@ -3,18 +3,17 @@ document.getElementById('loginForm').addEventListener('submit', function(e) {
     let username = document.getElementById('username').value;
     let password = document.getElementById('password').value;
 
-    // 检查凭据是否与存储在 localStorage 中的凭据匹配
     let storedUser = localStorage.getItem('username');
     let storedPass = localStorage.getItem('password');
 
     if (username === storedUser && password === storedPass) {
         alert('Login successful!');
-        localStorage.setItem('isLoggedIn', 'true'); // 设置登录状态
+        localStorage.setItem('isLoggedIn', 'true'); 
         let previousPage = localStorage.getItem('previousPage');
         if (previousPage) {
-            window.location.href = previousPage; // 重定向到用户之前访问的页面
+            window.location.href = previousPage; 
         } else {
-            window.location.href = 'index.html'; // 默认重定向到首页
+            window.location.href = 'index.html'; 
         }
     } else {
         alert('Invalid username or password.');
